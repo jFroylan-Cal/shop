7740a1a64259b599d984c77a57f1655a024b3bcb
jFroylan-Cal <j.froylan_calderon@outlook.com>
2023-04-20 09:41:54 -0600

    Add websocket to gateway

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,20 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messagesWsService: MessagesWsService) {}
+  handleConnection(client: Socket) {
+    console.log('Cliente Conectado', client.id)
+  }
+  handleDisconnect(client: any) {
+    console.log('Cliente desconectado', client.id)
+  }
 }
